b44a3f9bde657c9838c0b4d6b88ccdcc0cd092b2
Duong <thuyduongdinhthi2312@gmail.com>
2024-03-24 17:29:41 +0700

    feat: handle view user list

--- QuanTriTrongOracle/connect.cs
+++ QuanTriTrongOracle/connect.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
 
 namespace QuanTriTrongOracle
 {
     internal class connect
     {
+        private static OracleConnection connection;
         string PATH = "localhost:1521/XE;";
         static string conString = "";
+
+        public static OracleConnection getConnection()
+        {
+            {
+                // Kiểm tra nếu kết nối chưa được tạo hoặc đã đóng
+                if (connection == null)
+                {
+                    // Khởi tạo kết nối mới
+                    connection = new OracleConnection(conString);
+                }
+
+                return connection;
+            }
+        }
 
         public string getString()
         {
